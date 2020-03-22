a3ee06674ab85d88289ab564e7f67a8f99213758
Nguyen <nguyenj32@wit.edu>
2020-03-22 18:45:47 -0400

    Added Movie Insertions and changed Movie Table

---
+++ MovieInsert.sql
@@ -0,0 +1,75 @@
+#MOVIE_t
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV001', 'Avengers: Endgame', 2019, 'Action, Adventure, Sci-fi', '8.5/10',  181);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV002', 'Avatar', 2009, 'Action, Adventure, Fantasy, Sci-fi', '7.8/10',  182);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV003', 'Titanic', 1997, 'Drama, Romance, Thriller',  '7.8/10',  194);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV004', 'Star Wars: Episode VII - The Force Awakens', 2015, 'Action, Adventure, Fantasy, Sci-fi', '7.9/10',  136);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV005', 'Avengers: Infinity War', 2018, 'Action, Adventure, Sci-fi', '8.5/10',  149);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV006', 'Jurassic World', 2015, 'Action, Adventure, Sci-fi, Thriller', '7/10',  124);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV007', 'The Lion King', 2019, 'Adventure, Family', '7/10',  118);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV008', 'Marvels The Avengers', 2012, 'Action, Adventure, Sci-fi', '8/10',  143);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV009', 'Furious 7', 2015, 'Action, Crime, Thriller', '7.2/10',  137);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV010', 'Avengers: Age of Ultron', 2015, 'Action, Adventure, Sci-fi', '7.3/10',  141);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV011', 'Frozen II', 2019, 'Animation, Family, Adventure', '7.2/10',  104);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV012', 'Black Panther', 2018, 'Action, Adventure, Fantasy, Sci-fi', '7.3/10',  134);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV013', 'Harry Potter and the Deathly Hallows, Part 2', 2011, 'Adventure, Fantasy', '8.1/10',  130);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV014', 'Star Wars: Episode VIII - The Last Jedi', 2017, 'Action, Adventure, Fantasy, Sci-fi', '7/10',  152);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV015', 'Jurassic World: Fallen Kingdom', 2018, 'Action, Adventure, Sci-fi', '6.2/10',  129);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV016', 'Frozen', 2013, 'Adventure, Animation, Family', '7.5/10',  102);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV017', 'Beauty and the Beast', 2017, 'Family, Fantasy, Romance', '7.1/10',  129);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV018', 'Incredibles 2', 2018, 'Action, Adeventure, Animation, Family', '7.7/10',  118);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV019', 'The Fate of the Furious', 2017, 'Action, Crime, Thriller', '6.7/10',  136);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV020', 'Iron Man 3', 2013, 'Action, Adeventure, Sci-fi', '7.2/10',  131);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV021', 'Minions', 2015, 'Adventure, Animation, Comedy, Family', '6.4/10',  091);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV022', 'Captain America: Civil War', 2016, 'Action, Adventure, Sci-Fi', '7.8/10',  147);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV023', 'Aquaman', 2018, 'Action, Adventure, Fantasy', '7/10',  144);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV024', 'Spider-Man: Far from Home', 2019, 'Action, Adventure, Sci-fi', '7.6/10',  129);
+
+INSERT INTO `MOVIE_t`
+(`MovieID`, `Title`, `Year`, `Genre`, `Rating`, `Runtime`) VALUES ('MV025', 'Captain Marvel', 2019, 'Action, Adventure, Sci-fi', '6.9/10',  125);
