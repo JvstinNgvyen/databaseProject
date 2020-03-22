f2f3daeb0f5e432890f51a7bb237064a29518f49
colorussor <55548486+colorussor@users.noreply.github.com>
2020-03-22 17:25:26 -0400

    These are the insertions for Characters

---
+++ FINAL_Character_Instertions.sql
@@ -0,0 +1,312 @@
+# Characters
+
+# Avengers: Endgame
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV001');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0002','Captain America / Steve Rogers','MV001');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0003','Hulk / Bruce Banner','MV001');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0004','Thor','MV001');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0005','Blackwidow / Natasha Romanoff','MV001');
+
+
+# Avatar
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0006','Jake Sully','MV002');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0007','Princess Neytiri','MV002');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0008','Col. Quaritch','MV002');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0009','Dr. Grace Augustine','MV002');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0010','Norm Spellman','MV002');
+
+
+# Titanic
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0011','Rose Dewitt Bukater','MV003');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0012','Jack Dawson','MV003');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0013','Cal Hockley','MV003');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0014','Molly Brown','MV003');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0015','Ruth Dewitt Buakater','MV003');
+
+
+# Star Wars: Episode VII - The Force Awakens
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0016','Han Solo','MV004');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0017','Luke Skywalker','MV004');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0018','General Leia Organa','MV004');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0019','Kylo Ren / Ben Solo','MV004');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0020','Rey','MV004');
+
+
+#Avengers: Infinity War--------------Has repeats from Above
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV005');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0004','Thor','MV005');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0002','Captain America / Steve Rogers','MV005');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0003','Hulk / Bruce Banner','MV005');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0005','Blackwidow / Natasha Romanoff','MV005');
+
+
+#Jurassic World
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0021','Owen Grady','MV006');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0022','Claire Dearing','MV006');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0023','Simon Masrani','MV006');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0024','Vic Hoskins','MV006');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0025','Zach Mitchell','MV006');
+
+
+#The Lion King
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0026','Simba','MV007');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0027','Nala','MV007');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0028','Mufasa','MV007');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0029','Scar','MV007');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0030','Sarabi','MV007');
+
+
+#Marvel's The Avengers---------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV008');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0002','Captain America / Steve Rogers','MV008');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0003','Hulk / Bruce Banner','MV008');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0004','Thor','MV008');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0005','Blackwidow / Natasha Romanoff','MV008');
+
+
+#Furious 7
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0031','Dominic Toretto','MV009');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0032','Brian OConner','MV009');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0033','Luke Hobbs','MV009');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0034','Letty Ortiz','MV009');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0035','Roman Pearce','MV009');
+
+
+#Avengers: Age of Ultron------------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV010');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0002','Captain America / Steve Rogers','MV010');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0003','Hulk / Bruce Banner','MV010');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0004','Thor','MV010');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0005','Blackwidow / Natasha Romanoff','MV010');
+
+
+#Frozen II
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0036','Anna','MV011');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0037','Elsa','MV011');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0038','Kristoff','MV011');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0039','Olaf','MV011');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0040','Lieutenant Mattias','MV011');
+
+
+#Black Panther
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0041','King TChalla / Black Panther','MV012');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0042','NJadaka / Erik Killmonger Stevens','MV012');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0043','Nakia','MV012');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0044','Okoye','MV012');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0045','Everett K. Ross','MV012');
+
+
+#Harry Potter and the Deathly Hallows, Part 2
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0046','Harry Potter','MV013');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0047','Ron Weasley','MV013');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0048','Hermione Granger','MV013');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0049','Severus Snape','MV013');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0050','Lord Voldemort','MV013');
+
+
+#Star Wars: Episode VIII - The Last Jedi--------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0017','Luke Skywalker','MV014');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0018','General Leia Organa','MV014');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0019','Kylo Ren / Ben Solo','MV014');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0020','Rey','MV014');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0051','Finn','MV014');
+
+
+#Jurassic World: Fallen Kingdon-------------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0021','Owen Grady','MV015');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0022','Claire Dearing','MV015');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0052','Eli Mills','MV015');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0053','Franklin Webb','MV015');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0054','Zia Rodriguez','MV015');
+
+
+#Frozen
+INSERT INTO `CHARACTER_t`----------------
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0036','Anna','MV016');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0037','Elsa','MV016');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0038','Kristoff','MV016');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0039','Olaf','MV016');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0055','Hans','MV016');
+
+
+#Beauty and the Beast
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0056','Belle','MV017');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0057','Beast','MV017');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0058','Gaston','MV017');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0059','Maurice','MV017');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0060','Lefou','MV017');
+
+
+#Incredibles 2
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0061','Mr. Incredible / Bob Parr','MV018');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0062','Elastigirl / Helen Parr','MV018');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0063','Violet Parr','MV018');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0064','Jack-Jack Parr','MV018');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0065','Dash Parr','MV018');
+
+
+#The Fate of the Furious-------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0031','Dominic Toretto','MV019');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0066','Deckard Shaw','MV019');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0067','Luke Hobbs','MV019');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0034','Letty Ortiz','MV019');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0035','Roman Pearce','MV019');
+
+
+#Iron Man 3---------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV020');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0068','Virginia Potts','MV020');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0069','Iron Patriot / James Rhodes','MV020');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0070','Aldrich Killan','MV020');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0071','Maya Hansen','MV020');
+
+
+#Minions
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0072','Scarlet Overkill','MV021');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0073','Herb Overkill','MV021');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0074','Walter Nelson','MV021');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0075','Madge Nelson','MV021');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0076','Professor Flux / Tower Guard','MV021');
+
+
+#Captain America: Civil War-------------
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0002','Captain America / Steve Rogers','MV022');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0001','Iron Man / Tony Stark','MV022');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0005','Blackwidow / Natasha Romanoff','MV022');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0077','Winter Soldier / James Barnes','MV022');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0078','Falcon / Sam Wilson','MV022');
+
+
+#Aquaman
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0079','Aquaman / Arthur Curry','MV023');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0080','Mera','MV023');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0081','Nuidis Vulko','MV023');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0082','Ocean Master / King Orm','MV023');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0083','Atlanta','MV023');
+
+
+#Spider-Man: Far From Home
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0084','Spider-Man / Peter Parker','MV024');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0085','Nick Fury','MV024');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0086','Mysterio / Quentin Beck','MV024');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0087','May Parker','MV024');
+INSERT INTO `CHARACTER_t`
+(`CharacterID`, `Name`, `MovieID`) VALUES ('C0088','Harold Hogan','MV024');
