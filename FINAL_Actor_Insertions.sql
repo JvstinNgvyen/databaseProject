31d65d62939fadc051d4ffe8e89fca1e3ebf66d9
colorussor <55548486+colorussor@users.noreply.github.com>
2020-03-23 18:21:47 -0400

    ALL ACTOR INSERTIONS

---
+++ FINAL_Actor_Insertions.sql
@@ -0,0 +1,278 @@
+# Actors
+
+
+#CREATE TABLE ACTOR_t(
+#	ActorID 		INT(5) NOT NULL,
+#	Name 			VARCHAR(20),
+#	DOB 			TIMESTAMP,
+#	CharacterID		INT(5),
+
+#YYYY-MM-DD
+
+# Avengers: Endgame
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0001','Robert Downey Jr.','1965-04-04','C0001');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0002','Chris Evans','1981-06-13','C0002');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0003','Mark Ruffalo','1967-11-22','C0003');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0004','Chris Hemsworth','1983-08-11','C0004');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0005','Scarlett Johansson','1984-11-22','C0005');
+
+# Avatar
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0006','Sam Worthington','1967-08-02','C0006');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0007','Zoe Saldana','1978-06-19','C0007');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0008','Stephen Lang','1952-07-11','C0008');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0009','Sigourney Weaver','1949-10-08','C0009');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0010','Joel David Moore','1977-09-25','C0010');
+
+# Titanic
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0011','Kate Winslet','1975-10-05','C0011');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0012','Leonardo Decaprio','1974-11-11','C0012');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0013','Bill Zane','1966-02-24','C0013');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0014','Kathy Bates','1948-06-28','C0014');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0015','Frances Fisher','1952-05-11','C0015');
+
+# Star Wars: Episode VII - The Force Awakens
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0016','Harrison Ford','1942-07-13','C0016');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0017','Mark Hamill','1951-09-25','C0017');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0018','Carrie Fisher','1956-10-21','C0018');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0019','Adam Driver','1983-11-19','C0019');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0020','Daisey Ridley','1992-04-10','C0020');
+
+#Avengers: Infinity War-----
+#Robert Downey Jr.
+#Chris Hemsworth
+#Chris Evans
+#Mark Ruffalo
+#Scarlet Johansson
+
+#Jurassic World
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0021','Chris Pratt','1979-06-21','C0021');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0022','Bryce Dallas Howard','1981-03-02','C0022');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0023','Irrfan Khan','1967-01-07','C0023');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0024','Vincent D Onofrio','1959-06-30','C0024');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0025','Nick Robinson','1995-03-22','C0025');
+
+
+#The Lion King
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0026','Donald Glover','1983-09-25','C0026');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0027','Beyonce Knowles','1981-09-04','C0027');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0028','James Earl Jones','1931-01-17','C0028');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0029','Chiwetel Ejiofor','1977-07-10','C0029');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0030','Alfre Woodward','1952-11-08','C0030');
+
+#Marvel's the Avengers------
+#RDJ
+#Chris Evans
+#Mark Ruffalo
+#Chris Hemsworth
+#Scarlet Johansson
+
+#Furious 7
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0031','Vin Diesel','1967-07-18','C0031');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0032','Paul Walker','1973-09-12','C0032');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0033','Dwayne Johnson','1972-05-02','C0033');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0034','Michelle Rodriguez','1978-07-12','C0034');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0035','Tyrese Gibson','1978-12-30','C0035');
+
+#Avengers Age of Ultron-------------
+#RDJ
+#Chris Evans
+#Mark Ruffalo
+#Chris Hemsworth
+#Scarlet Johansson
+
+#Frozen II
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0036','Kristen Bell','1980-07-18','C0036');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0037','Idina Menzel','1971-05-30','C0037');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0038','Jonathan Groff','1985-03-26','C0038');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0039','Josh Gad','1981-02-23','C0039');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0040','Sterling K. Brown','1976-04-05','C0040');
+
+#Black Panther
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0041','Chadwick Boseman','1976-11-29','C0041');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0042','Michael B. Jordan','1987-02-09','C0042');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0043','Lupita Nyongo','1983-03-01','C0043');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0044','Danai Gurira','1978-02-14','C0044');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0045','Martin Freeman','1971-09-08','C0045');
+
+#Harry Potter And the Deathly Hallows, Part 2
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0046','Daniel Radcliffe','1989-07-23','C0046');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0047','Rupert Grint','1988-08-24','C0047');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0048','Emma Watson','1990-04-15','C0048');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0049','Alan Rickman','1946-02-21','C0049');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0050','Ralph Fiennes','1962-12-22','C0050');
+
+#Star Wars: Episode VIII - The Last Jedi------------------
+#Mark Hamill
+#Carrie Fisher
+#Adam Driver
+#Daisey Ridley
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0051','John Boyega','1992-03-17','C0051');
+
+#Jurassic World: Fallen Kingdom---------------------
+#Chris Pratt
+#Bryce Dallas Howard
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0052',' Rafe Spall','1983-03-10','C0052');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0053','Justice Smith','1995-08-09','C0053');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0054','Daniella Pineda','1987-02-20','C0054');
+
+#Frozen---------------
+#Kristen Bell
+#Idina Menzel
+#Jonathan Groff
+#Josh Gad
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0055','Santino Fontana','1982-03-21','C0055');
+
+#Beauty and the Beast
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0056','Emma Watson','1990-04-15','C0056');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0057','Dan Stevens','1982-10-10','C0057');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0058','Luke Evans','1979-04-15','C0058');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0059','Kevin Kline','1947-10-24','C0059');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0060','Josh Gad','1981-02-23','C0060');
+
+#Incredibles 2
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0061','Craig T. Nelson','1944-04-04','C0061');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0062','Holly Hunter','1958-03-20','C0062');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0063','Sarah Vowell','1969-12-27','C0063');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0064','Eli Fucile','2002-?-?','C0064');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0065','Huck Milner','2008-08-30','C0065');
+
+#The Fate of the Furious----------
+#Vin Diesel
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0066','Jason Statham','1967-07-26','C0066');
+#Dwayne Johnson
+#Michelle Rodriguez
+#Tyrese Gibson
+
+#Iron Man 3-----------
+#Robert Downey Jr.
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0067','Gwyneth Paltrow','1972-09-27','C0068');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0068','Don Cheadle','1964-11-29','C0069');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0069','Guy Pearce','1967-10-05','C0070');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0070','Rebecca Hall','1982-05-03','C0071');
+
+#Minions
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0071','Sandra Bullock','1964-07-26','C0072');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0072','Jon Hamm','1971-03-10','C0073');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0073','Michael Keaton','1951-09-05','C0074');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0074','Allison Janney','1959-11-19','C0075');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0075','Steve Coogan','1965-10-14','C0076');
+
+#Captain America: Civil War----------
+#Chris Evans
+#Robert Downey Jr.
+#Scarlett Johansson
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0076','Sebastian Stan','1982-08-13','C0077');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0077','Anthony Mackie','1979-09-23','C0078');
+
+#Aquaman
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0078','Jason Momoa','1979-08-01','C0079');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0079','Amber Heard','1986-04-22','C0080');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0080','Willem Dafoe','1955-07-22','C0081');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0081','Patrick Wilson','1973-07-03','C0082');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0082','Nicole Kidman','1967-06-20','C0083');
+
+#Spider-Man: Far Fom Home
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0083','Tom Holland','1996-06-01','C0084');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0084','Samuel L. Jackson','1948-12-21','C0085');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0085','Jake Gyllenhaal','1980-12-19','C0086');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0086','Marisa Tomei','1964-12-04','C0087');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0087','Jon Favreau','1966-10-19','C0088');
+
+#Captain Marvel
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0088','Brie Larson','1989-10-01','C0089');
+#Samuel L. Jackson???????????????????????????
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0089','Ben Mendelsohn','1969-04-03','C0091');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0090','Jude Law','1972-12-29','C0092');
+INSERT INTO `ACTOR_t`
+(`ActorID`, `Name`, `DOB`, `CharacterID`) VALUES ('A0091','Annette Bening','1958-05-29','C0093');
+
