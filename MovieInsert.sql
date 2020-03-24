391325ab18ce47a89f54b263dc5bff24a56c8020
Nguyen <nguyenj32@wit.edu>
2020-03-23 21:49:11 -0400

    Added Played_t Insert

--- MovieInsert.sql
+++ MovieInsert.sql
@@ -76,76 +76,393 @@ INSERT INTO `MOVIE_t`
 
 #MONEY_t
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN001', 356000000, 2797800564, 'MV001');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN001', 356000000, 2797800564, 'MV001');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN002', 237000000, 2789679794, 'MV002');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN002', 237000000, 2789679794, 'MV002');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN003', 200000000, 2187463944, 'MV003');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN003', 200000000, 2187463944, 'MV003');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN004', 245000000, 2068223624, 'MV004');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN004', 245000000, 2068223624, 'MV004');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN005', 300000000, 2048359754, 'MV005');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN005', 300000000, 2048359754, 'MV005');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN006', 150000000, 1671713208, 'MV006');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN006', 150000000, 1671713208, 'MV006');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN007', 260000000, 1656943394, 'MV007');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN007', 260000000, 1656943394, 'MV007');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN008', 220000000, 1518812988, 'MV008');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN008', 220000000, 1518812988, 'MV008');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN009', 190000000, 1516045911, 'MV009');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN009', 190000000, 1516045911, 'MV009');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN010', 280000000, 1405403694, 'MV010');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN010', 280000000, 1405403694, 'MV010');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN011', 33000000, 1373115944, 'MV011');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN011', 33000000, 1373115944, 'MV011');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN012', 200000000, 1346913161, 'MV012');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN012', 200000000, 1346913161, 'MV012');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN013', 125000000, 1341693157, 'MV013');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN013', 125000000, 1341693157, 'MV013');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN014', 200000000, 1332539889, 'MV014');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN014', 200000000, 1332539889, 'MV014');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN015', 170000000, 1309484461, 'MV015');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN015', 170000000, 1309484461, 'MV015');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN016', 150000000, 1290000000, 'MV016');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN016', 150000000, 1290000000, 'MV016');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN017', 160000000, 1263521126, 'MV017');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN017', 160000000, 1263521126, 'MV017');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN018', 200000000, 1242805359, 'MV018');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN018', 200000000, 1242805359, 'MV018');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN019', 250000000, 250000000, 'MV019');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN019', 250000000, 250000000, 'MV019');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN020', 200000000, 1214811252, 'MV020');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN020', 200000000, 1214811252, 'MV020');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN021', 74000000, 1159398397, 'MV021');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN021', 74000000, 1159398397, 'MV021');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN022', 240000000, 1153304495, 'MV022');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN022', 240000000, 1153304495, 'MV022');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN023', 160000000, 1148161807, 'MV023');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN023', 160000000, 1148161807, 'MV023');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN024', 160000000, 1131927996, 'MV024');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN024', 160000000, 1131927996, 'MV024');
 
 INSERT INTO `MONEY_t`
-(`MONEYID`, `Budget`, `Income`, `MovieID`) VALUES ('MN025', 152000000, 1128274794, 'MV025');
+(`MoneyID`, `Budget`, `Income`, `MovieID`) VALUES ('MN025', 152000000, 1128274794, 'MV025');
+
+
+#Studios
+INSERT INTO studio_t VALUES ('ST001', 'Marvel Studios', '1993');
+INSERT INTO studio_t VALUES ('ST002', 'Twentieth Century Fox', '1935');
+INSERT INTO studio_t VALUES ('ST003', 'Paramount', '1912');
+INSERT INTO studio_t VALUES ('ST004', 'Lucasfilms', '1971');
+INSERT INTO studio_t VALUES ('ST005', 'Universal Pictures', '1912');
+INSERT INTO studio_t VALUES ('ST006', 'Disney Studios', '1923');
+INSERT INTO studio_t VALUES ('ST007', 'Warner Bros.', '1923');
+INSERT INTO studio_t VALUES ('ST008', 'Sony Pictures', '1987');
+
+#USES
+INSERT INTO uses_t VALUES ('MV001', 'ST001');
+INSERT INTO uses_t VALUES ('MV002', 'ST002');
+INSERT INTO uses_t VALUES ('MV003', 'ST003');
+INSERT INTO uses_t VALUES ('MV003', 'ST002');
+INSERT INTO uses_t VALUES ('MV004', 'ST004');
+INSERT INTO uses_t VALUES ('MV005', 'ST002');
+INSERT INTO uses_t VALUES ('MV006', 'ST005');
+INSERT INTO uses_t VALUES ('MV007', 'ST006');
+INSERT INTO uses_t VALUES ('MV008', 'ST001');
+INSERT INTO uses_t VALUES ('MV009', 'ST005');
+INSERT INTO uses_t VALUES ('MV010', 'ST001');
+INSERT INTO uses_t VALUES ('MV011', 'ST006');
+INSERT INTO uses_t VALUES ('MV012', 'ST001');
+INSERT INTO uses_t VALUES ('MV013', 'ST007');
+INSERT INTO uses_t VALUES ('MV014', 'ST004');
+INSERT INTO uses_t VALUES ('MV015', 'ST006');
+INSERT INTO uses_t VALUES ('MV016', 'ST006');
+INSERT INTO uses_t VALUES ('MV017', 'ST006');
+INSERT INTO uses_t VALUES ('MV018', 'ST006');
+INSERT INTO uses_t VALUES ('MV019', 'ST005');
+INSERT INTO uses_t VALUES ('MV020', 'ST001');
+INSERT INTO uses_t VALUES ('MV021', 'ST005');
+INSERT INTO uses_t VALUES ('MV022', 'ST001');
+INSERT INTO uses_t VALUES ('MV023', 'ST007');
+INSERT INTO uses_t VALUES ('MV024', 'ST001');
+INSERT INTO uses_t VALUES ('MV024', 'ST008');
+INSERT INTO uses_t VALUES ('MV025', 'ST001');
+
+#Director
+INSERT INTO director_t VALUES ('D0001', 'Joe Russo', '1971-07-08');
+INSERT INTO director_t VALUES ('D0002', 'Anthony Russo', '1970-02-03');
+INSERT INTO director_t VALUES ('D0003', 'James Cameron', '1954-08-16');
+INSERT INTO director_t VALUES ('D0004', 'J.J Abrams', '1966-06-27');
+INSERT INTO director_t VALUES ('D0005', 'Colin Trevorrow', '1976-09-13');
+INSERT INTO director_t VALUES ('D0006', 'Jon Favreau', '1966-10-19');
+INSERT INTO director_t VALUES ('D0007', 'Joss Whedon', '1964-06-23');
+INSERT INTO director_t VALUES ('D0008', 'James Wan', '1977-02-27');
+INSERT INTO director_t VALUES ('D0009', 'Jennifer Lee', '1971-10-22');
+INSERT INTO director_t VALUES ('D0010', 'Chris Buck', '1960-10-25');
+INSERT INTO director_t VALUES ('D0011', 'Ryan Coogler', '1986-05-23');
+INSERT INTO director_t VALUES ('D0012', 'David Yates', '1963-11-30');
+INSERT INTO director_t VALUES ('D0013', 'Rian Johnson', '1973-12-17');
+INSERT INTO director_t VALUES ('D0014', 'J. A. Bayona', '1975-05-09');
+INSERT INTO director_t VALUES ('D0015', 'Bill Condon', '1955-10-22');
+INSERT INTO director_t VALUES ('D0016', 'Brad Bird', '1957-09-11');
+INSERT INTO director_t VALUES ('D0017', 'Rob Cohen', '1949-03-12');
+INSERT INTO director_t VALUES ('D0018', 'Shane Black', '1961-12-16');
+INSERT INTO director_t VALUES ('D0019', 'Pierre Coffin', '1967-03-16');
+INSERT INTO director_t VALUES ('D0020', 'Kyle Balda', '1900-01-01');
+INSERT INTO director_t VALUES ('D0021', 'Jon Watts', '1981-06-28');
+INSERT INTO director_t VALUES ('D0022', 'Anna Boden', '1976-09-20');
+INSERT INTO director_t VALUES ('D0023', 'Ryan Fleck', '1976-09-20');
+
+#Directs
+INSERT INTO directs_t VALUES ('MV001', 'D0001');
+INSERT INTO directs_t VALUES ('MV001', 'D0002');
+INSERT INTO directs_t VALUES ('MV002', 'D0003');
+INSERT INTO directs_t VALUES ('MV003', 'D0003');
+INSERT INTO directs_t VALUES ('MV004', 'D0004');
+INSERT INTO directs_t VALUES ('MV005', 'D0001');
+INSERT INTO directs_t VALUES ('MV005', 'D0002');
+INSERT INTO directs_t VALUES ('MV006', 'D0005');
+INSERT INTO directs_t VALUES ('MV007', 'D0006');
+INSERT INTO directs_t VALUES ('MV008', 'D0007');
+INSERT INTO directs_t VALUES ('MV009', 'D0008');
+INSERT INTO directs_t VALUES ('MV010', 'D0007');
+INSERT INTO directs_t VALUES ('MV011', 'D0009');
+INSERT INTO directs_t VALUES ('MV011', 'D0010');
+INSERT INTO directs_t VALUES ('MV012', 'D0011');
+INSERT INTO directs_t VALUES ('MV013', 'D0012');
+INSERT INTO directs_t VALUES ('MV014', 'D0013');
+INSERT INTO directs_t VALUES ('MV015', 'D0014');
+INSERT INTO directs_t VALUES ('MV016', 'D0009');
+INSERT INTO directs_t VALUES ('MV016', 'D0010');
+INSERT INTO directs_t VALUES ('MV017', 'D0015');
+INSERT INTO directs_t VALUES ('MV018', 'D0016');
+INSERT INTO directs_t VALUES ('MV019', 'D0017');
+INSERT INTO directs_t VALUES ('MV020', 'D0018');
+INSERT INTO directs_t VALUES ('MV021', 'D0019');
+INSERT INTO directs_t VALUES ('MV021', 'D0020');
+INSERT INTO directs_t VALUES ('MV022', 'D0001');
+INSERT INTO directs_t VALUES ('MV022', 'D0002');
+INSERT INTO directs_t VALUES ('MV023', 'D0008');
+INSERT INTO directs_t VALUES ('MV024', 'D0021');
+INSERT INTO directs_t VALUES ('MV025', 'D0022');
+INSERT INTO directs_t VALUES ('MV025', 'D0023');
+
+
+
+
+#Played_t
+INSERT INTO Played_t VALUES ('MV001', 'C0001');
+INSERT INTO Played_t VALUES ('MV001', 'C0002');
+INSERT INTO Played_t VALUES ('MV001', 'C0003');
+INSERT INTO Played_t VALUES ('MV001', 'C0004');
+INSERT INTO Played_t VALUES ('MV001', 'C0005');
+
+INSERT INTO Played_t VALUES ('MV002', 'C0006');
+INSERT INTO Played_t VALUES ('MV002', 'C0007');
+INSERT INTO Played_t VALUES ('MV002', 'C0008');
+INSERT INTO Played_t VALUES ('MV002', 'C0009');
+INSERT INTO Played_t VALUES ('MV002', 'C0010');
+
+INSERT INTO Played_t VALUES ('MV003', 'C0011');
+INSERT INTO Played_t VALUES ('MV003', 'C0012');
+INSERT INTO Played_t VALUES ('MV003', 'C0013');
+INSERT INTO Played_t VALUES ('MV003', 'C0014');
+INSERT INTO Played_t VALUES ('MV003', 'C0015');
+
+INSERT INTO Played_t VALUES ('MV004', 'C0016');
+INSERT INTO Played_t VALUES ('MV004', 'C0017');
+INSERT INTO Played_t VALUES ('MV004', 'C0018');
+INSERT INTO Played_t VALUES ('MV004', 'C0019');
+INSERT INTO Played_t VALUES ('MV004', 'C0020');
+
+INSERT INTO Played_t VALUES ('MV005', 'C0001');
+INSERT INTO Played_t VALUES ('MV005', 'C0004');
+INSERT INTO Played_t VALUES ('MV005', 'C0002');
+INSERT INTO Played_t VALUES ('MV005', 'C0003');
+INSERT INTO Played_t VALUES ('MV005', 'C0005');
+
+INSERT INTO Played_t VALUES ('MV006', 'C0021');
+INSERT INTO Played_t VALUES ('MV006', 'C0022');
+INSERT INTO Played_t VALUES ('MV006', 'C0023');
+INSERT INTO Played_t VALUES ('MV006', 'C0024');
+INSERT INTO Played_t VALUES ('MV006', 'C0025');
+
+INSERT INTO Played_t VALUES ('MV007', 'C0026');
+INSERT INTO Played_t VALUES ('MV007', 'C0027');
+INSERT INTO Played_t VALUES ('MV007', 'C0028');
+INSERT INTO Played_t VALUES ('MV007', 'C0029');
+INSERT INTO Played_t VALUES ('MV007', 'C0030');
+
+INSERT INTO Played_t VALUES ('MV008', 'C0001');
+INSERT INTO Played_t VALUES ('MV008', 'C0002');
+INSERT INTO Played_t VALUES ('MV008', 'C0003');
+INSERT INTO Played_t VALUES ('MV008', 'C0004');
+INSERT INTO Played_t VALUES ('MV008', 'C0005');
+
+INSERT INTO Played_t VALUES ('MV009', 'C0031');
+INSERT INTO Played_t VALUES ('MV009', 'C0032');
+INSERT INTO Played_t VALUES ('MV009', 'C0033');
+INSERT INTO Played_t VALUES ('MV009', 'C0034');
+INSERT INTO Played_t VALUES ('MV009', 'C0035');
+
+INSERT INTO Played_t VALUES ('MV010', 'C0001');
+INSERT INTO Played_t VALUES ('MV010', 'C0002');
+INSERT INTO Played_t VALUES ('MV010', 'C0003');
+INSERT INTO Played_t VALUES ('MV010', 'C0004');
+INSERT INTO Played_t VALUES ('MV010', 'C0005');
+
+INSERT INTO Played_t VALUES ('MV011', 'C0036');
+INSERT INTO Played_t VALUES ('MV011', 'C0037');
+INSERT INTO Played_t VALUES ('MV011', 'C0038');
+INSERT INTO Played_t VALUES ('MV011', 'C0039');
+INSERT INTO Played_t VALUES ('MV011', 'C0040');
+
+INSERT INTO Played_t VALUES ('MV012', 'C0041');
+INSERT INTO Played_t VALUES ('MV012', 'C0042');
+INSERT INTO Played_t VALUES ('MV012', 'C0043');
+INSERT INTO Played_t VALUES ('MV012', 'C0044');
+INSERT INTO Played_t VALUES ('MV012', 'C0045');
+
+INSERT INTO Played_t VALUES ('MV013', 'C0046');
+INSERT INTO Played_t VALUES ('MV013', 'C0047');
+INSERT INTO Played_t VALUES ('MV013', 'C0048');
+INSERT INTO Played_t VALUES ('MV013', 'C0049');
+INSERT INTO Played_t VALUES ('MV013', 'C0050');
+
+INSERT INTO Played_t VALUES ('MV014', 'C0017');
+INSERT INTO Played_t VALUES ('MV014', 'C0018');
+INSERT INTO Played_t VALUES ('MV014', 'C0019');
+INSERT INTO Played_t VALUES ('MV014', 'C0020');
+INSERT INTO Played_t VALUES ('MV014', 'C0051');
+
+INSERT INTO Played_t VALUES ('MV015', 'C0021');
+INSERT INTO Played_t VALUES ('MV015', 'C0022');
+INSERT INTO Played_t VALUES ('MV015', 'C0052');
+INSERT INTO Played_t VALUES ('MV015', 'C0053');
+INSERT INTO Played_t VALUES ('MV015', 'C0054');
+
+INSERT INTO Played_t VALUES ('MV016', 'C0036');
+INSERT INTO Played_t VALUES ('MV016', 'C0037');
+INSERT INTO Played_t VALUES ('MV016', 'C0038');
+INSERT INTO Played_t VALUES ('MV016', 'C0039');
+INSERT INTO Played_t VALUES ('MV016', 'C0055');
+
+INSERT INTO Played_t VALUES ('MV017', 'C0056');
+INSERT INTO Played_t VALUES ('MV017', 'C0057');
+INSERT INTO Played_t VALUES ('MV017', 'C0058');
+INSERT INTO Played_t VALUES ('MV017', 'C0059');
+INSERT INTO Played_t VALUES ('MV017', 'C0060');
+
+INSERT INTO Played_t VALUES ('MV018', 'C0061');
+INSERT INTO Played_t VALUES ('MV018', 'D0062');
+INSERT INTO Played_t VALUES ('MV018', 'C0063');
+INSERT INTO Played_t VALUES ('MV018', 'C0064');
+INSERT INTO Played_t VALUES ('MV018', 'C0065');
+
+INSERT INTO Played_t VALUES ('MV019', 'C0031');
+INSERT INTO Played_t VALUES ('MV019', 'C0066');
+INSERT INTO Played_t VALUES ('MV019', 'C0033');
+INSERT INTO Played_t VALUES ('MV019', 'C0034');
+INSERT INTO Played_t VALUES ('MV019', 'C0035');
+
+INSERT INTO Played_t VALUES ('MV020', 'C0001');
+INSERT INTO Played_t VALUES ('MV020', 'C0068');
+INSERT INTO Played_t VALUES ('MV020', 'C0069');
+INSERT INTO Played_t VALUES ('MV020', 'C0070');
+INSERT INTO Played_t VALUES ('MV020', 'C0071');
+
+INSERT INTO Played_t VALUES ('MV021', 'C0072');
+INSERT INTO Played_t VALUES ('MV021', 'C0073');
+INSERT INTO Played_t VALUES ('MV021', 'C0074');
+INSERT INTO Played_t VALUES ('MV021', 'C0075');
+INSERT INTO Played_t VALUES ('MV021', 'C0076');
+
+INSERT INTO Played_t VALUES ('MV022', 'C0002');
+INSERT INTO Played_t VALUES ('MV022', 'C0001');
+INSERT INTO Played_t VALUES ('MV022', 'C0005');
+INSERT INTO Played_t VALUES ('MV022', 'C0077');
+INSERT INTO Played_t VALUES ('MV022', 'C0078');
+
+INSERT INTO Played_t VALUES ('MV023', 'C0079');
+INSERT INTO Played_t VALUES ('MV023', 'C0080');
+INSERT INTO Played_t VALUES ('MV023', 'C0081');
+INSERT INTO Played_t VALUES ('MV023', 'C0082');
+INSERT INTO Played_t VALUES ('MV023', 'C0083');
+
+INSERT INTO Played_t VALUES ('MV024', 'C0084');
+INSERT INTO Played_t VALUES ('MV024', 'C0085');
+INSERT INTO Played_t VALUES ('MV024', 'C0086');
+INSERT INTO Played_t VALUES ('MV024', 'C0087');
+INSERT INTO Played_t VALUES ('MV024', 'C0088');
+
+INSERT INTO Played_t VALUES ('MV025', 'C0089');
+INSERT INTO Played_t VALUES ('MV025', 'C0090');
+INSERT INTO Played_t VALUES ('MV025', 'C0091');
+INSERT INTO Played_t VALUES ('MV025', 'C0092');
+INSERT INTO Played_t VALUES ('MV025', 'C0093');
+
+#AWARD_t
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW001', 'Peoples Choice Award for Favorite Movie Actor', 'MV001','A0001');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW002', 'Empire Award for Best Actress', 'MV002','A0007');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW003', 'Award for Favorite Actor', 'MV003','A0012');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW004', 'MTV Movie Award for Best Breakthrough Performance', 'MV004','A0020');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW005', 'Teen Choice Award for Choice Movie Actor', 'MV005','A0001');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW006', 'MTV Movie Award for Best Action Sequence', 'MV006', 'A0021');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW007', 'Peoples Choice Award for Favorite Movie Actor', 'MV008','A0001');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW008', 'Teen Choice Award for Choice Movie Actor', 'MV009','A0032');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW009', 'Best Global Actress in a Motion Picture', 'MV010','A0005');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW010', 'Best Animated Female', 'MV011','A0036');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW011', 'MTV Movie Award for Best Villain', 'MV012','A0042');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW013', 'MTV Movie Award for Best Hero', 'MV013','A0046');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW014', 'Empire Award for Best Actress', 'MV015','A0020');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW015', 'Teen Choice Award for Choice Summer Movie Star', 'MV016','A0021');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW016', 'Best Animated Female', 'MV017','A0036');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW017', 'Teen Choice Award for Choice Movie Actress', 'MV018','A0056');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW018', 'Best Animated Female', 'MV019','A0062');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW019', 'Teen Choice Award for Choice Movie Actor', 'MV021','A0001');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW020', 'Best Female Lead Vocal Performance in a Feature Film', 'MV022','A0071');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW021', 'Teen Choice Award for Choice Movie Actor', 'MV023','A0002');
+
+INSERT INTO `AWARD_t`
+(`AwardID`, `Prize`, `MovieID`, `ActorID`) VALUES ('AW022', 'Teen Choice Award for Choice Summer Movie Star', 'MV024','A0083');
